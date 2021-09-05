469863f6c56402852e4c127b3e88262a7d5a619c
mahonec <mahonec@clarkson.edu>
2021-09-05 11:46:57 -0400

    add webhook server

---
+++ webhook/README.md
@@ -0,0 +1,11 @@
+Setup:
+Write the gitea git hook secret to `.env`
+```
+SECRET=ABCDEF
+```
+
+Run:
+From this directory run:
+```
+sudo npm start
+```
---
+++ webhook/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const express = require("express");
+const { exec } = require("child_process");
+const app = express();
+app.use(express.json())
+
+app.post('/', (req, res) => {
+    if (req.body.secret != process.env.SECRET) {
+        res.sendStatus(403);
+    }
+
+    exec("cd .. && git reset --hard && git pull");
+
+    exec("systemctl reload nsd", (error, stdout, stderr) => {
+        if (error) {
+            res.sendStatus(500);
+        } else {
+            res.sendStatus(200);
+        }
+    });
+})
+
+app.listen(80, () => {
+    console.log(`Example app listening at http://localhost:80`)
+})
